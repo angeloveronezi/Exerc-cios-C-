88a584c53fbec7f8e990ea740a37d9fc404da303
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-19 22:13:54 -0300

    Add files via upload
    Projeto para alterar dias em segundos

---
+++ DiasEmSegundos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace DiasEmSegundos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de dia(s): ");
+            int valor = int.Parse(Console.ReadLine());
+
+            int dia = (60 * 60 * 24) * valor;
+            Console.WriteLine(valor + " dias em segundos: " + dia);
+        }
+    }
+}
