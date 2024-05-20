48e306a52bc9887afcfd7c98a9ae4c4d7ccd8c75
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 19:59:21 -0300

    Add files via upload

---
+++ Exercicio 1008/Exercicio1008_Judge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1008_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1008
+             * 
+             */
+
+            int codFuncionario, horaTrabalhada;
+            double recebeHora;
+            codFuncionario = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            horaTrabalhada = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            recebeHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NUMBER = " + codFuncionario);
+            Console.WriteLine("SALARY = U$ " + (horaTrabalhada * recebeHora).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicio 1009/Exercicio1009_Judge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1009_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1009
+             * 
+             */
+
+            string NomeFunc = "";
+            double SalarioFixo, TotalVendas, TotalSalario;
+
+            NomeFunc = Console.ReadLine();
+            SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            TotalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            TotalSalario = SalarioFixo + (TotalVendas * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + TotalSalario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
