9da27564985128ea9e7b58d9c19f256339472ead
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 20:49:10 -0300

    Add files via upload

---
+++ Exercicio 1014/Exercicio1014_Judge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1014_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1014
+             * 
+             */
+            int X = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine((X/Y).ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
