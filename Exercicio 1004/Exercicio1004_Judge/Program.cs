f83f5100fb6bd40d973752df58635d4abd984dc5
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 19:52:12 -0300

    Add files via upload

---
+++ Exercicio 1004/Exercicio1004_Judge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1004_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1004
+             * 
+             */
+
+            int x, y, PROD;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            PROD = x * y;
+
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
---
+++ Exercicio 1005/Exercicio1005_Judge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1005_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1005
+             * 
+             */
+
+            double A, B, MEDIA;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            MEDIA = (A * (3.5 / 11)) + (B * (7.5 / 11));
+            //Utilizado 11 pois o peso é total é 11 (3.5 + 7.5)
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
