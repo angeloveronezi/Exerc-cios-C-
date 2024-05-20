0a0b35cf27663f7c0ef85f8f62533392676b019c
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 20:17:40 -0300

    Add files via upload

---
+++ Exercicio 1010/Exercicio1010_Judge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1010_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1010
+             * 
+             */
+
+            string[] pecas_1 = Console.ReadLine().Split(' ');
+            string[] pecas_2 = Console.ReadLine().Split(' ');
+
+            int CodPecas_1 = int.Parse(pecas_1[0]);
+            int QuantidadePecas_1 = int.Parse(pecas_1[1]);
+            double ValorPecas_1 = double.Parse(pecas_1[2], CultureInfo.InvariantCulture);
+            
+            double TotalPagarPecas_1 = QuantidadePecas_1 * ValorPecas_1;
+
+
+            int CodPecas_2 = int.Parse(pecas_2[0]);
+            int QuantidadePecas_2 = int.Parse(pecas_2[1]);
+            double ValorPecas_2 = double.Parse(pecas_2[2], CultureInfo.InvariantCulture);
+
+            double TotalPagarPecas_2 = QuantidadePecas_2 * ValorPecas_2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + (TotalPagarPecas_1 + TotalPagarPecas_2).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Exercicio 1011/Exercicio1011_Judge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio1011_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1011
+             * 
+             */
+            double pi = 3.14159;
+            double formulaRaio = (4.0 / 3.0) * pi;
+            float raio = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("VOLUME = " + (formulaRaio * (Math.Pow(raio, 3))).ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
