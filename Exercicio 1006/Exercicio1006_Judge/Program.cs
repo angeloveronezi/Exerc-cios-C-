84687e1b924370324f32fbedb30989f877369aa5
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 19:55:32 -0300

    Add files via upload

---
+++ Exercicio 1006/Exercicio1006_Judge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1006_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercício 1006
+             */
+
+            double A, B, C;
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Exemplo:
+            // Console.WriteLine("A = " + (A + 1).ToString("F3", CultureInfo.InvariantCulture));
+            // Console.WriteLine("B = " + (B + 2).ToString("F2", CultureInfo.InvariantCulture));
+            // Console.WriteLine("C = " + (C - 4).ToString("F1", CultureInfo.InvariantCulture));
+
+            // Conta da primeira - Se 10.0 = 2 e 5.0 = 0,5 então regra de três (10 = 2 --- A = X)
+
+            Console.WriteLine("MEDIA = " + (((A * 2) / 10) + ((B * 3) / 10) + ((C * 5) / 10)).ToString("F1", CultureInfo.InvariantCulture));
+
+            
+        }
+    }
+}
---
+++ Exercicio 1007/Exercicio1007_Judge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1007_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1007
+             * 
+             */
+
+            int A, B, C, D;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIFERENCA = " + ((A * B) - (C * D)));
+        }
+    }
+}
