a4dbf8f0f6e5ad2400920d9d6cfb15ad09fe5eae
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 21:02:39 -0300

    Add files via upload

---
+++ Exercicio 1016/Exercicio1016_Judge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1016_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1016
+             * 
+             */
+
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((num * 2) + " minutos");
+        }
+    }
+}
