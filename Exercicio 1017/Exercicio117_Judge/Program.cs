dd951dfd40d75fca1b4bc1896bedad8d08cd79c2
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 21:09:32 -0300

    Add files via upload

---
+++ Exercicio 1017/Exercicio117_Judge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio117_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1017
+             * 
+             */
+            int tempo = int.Parse(Console.ReadLine());
+            int km = int.Parse(Console.ReadLine());
+
+            double total = (Convert.ToDouble(km) / 12) * tempo;
+
+            Console.WriteLine(total.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
