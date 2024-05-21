ba4710feaae191f01dcf11b91dafcc03dfa1a1c5
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 21:55:33 -0300

    Add files via upload

---
+++ Exercício 1036/Exercicio1036_Judge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercício 1036
+             * https://judge.beecrowd.com/pt
+             * Realizando a fórmula de bhaskara referente a equação do segundo grau;
+             *
+             */
+
+            string[] valores = Console.ReadLine().Split(" ");
+            double A, B, C, delta;
+            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            B = Convert.ToDouble(valores[1], CultureInfo.InvariantCulture);
+            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            delta = (Math.Pow(B, 2)) - (4 * A * C);
+            if (A <= 0 || delta <= 0)
+            {
+                Console.WriteLine("Impossível calcular");
+            }
+            else
+            {
+                delta = Math.Sqrt(delta);
+                Console.WriteLine("R1 = " + (((-B) + delta) / (2 * A)).ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + (((-B) - delta) / (2 * A)).ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
