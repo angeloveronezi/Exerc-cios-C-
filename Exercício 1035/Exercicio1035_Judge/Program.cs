1c31d2ec05a6c941cfe82793f8924987f896aeea
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 21:24:34 -0300

    Add files via upload

---
+++ Exercício 1035/Exercicio1035_Judge/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio1035_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * https://judge.beecrowd.com/pt
+            * 
+            * Exercícío 1035
+            *
+            */
+            
+            int A, B, C, D, X;
+            string[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+            D = int.Parse(vet[3]);
+            X = A % 2;
+
+            if ((A >= 0) && (B >= 0) && (C >= 0) && (D >= 0))
+            {
+                if (B > C)
+                {
+                    Console.WriteLine("Valores aceitos");
+                }
+                else if (B > C)
+                {
+                    Console.WriteLine("Valores aceitos");
+                }
+                else if ((C + D) > (A + B + C + D))
+                {
+                    Console.WriteLine("Valores aceitos");
+                }
+                else if (X == 0)
+                {
+                    Console.WriteLine("Valores aceitos");
+                }
+                else
+                {
+                    Console.WriteLine("Valores nao aceitos");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
