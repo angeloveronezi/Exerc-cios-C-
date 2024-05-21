573c12494454b5e1c4e34423df3a6b9920e4b8ab
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 22:08:12 -0300

    Add files via upload

---
+++ Exercício 1044/Exercicio1044_Judge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio1044_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1044
+             *
+             */
+            int A, B;
+            string[] vet = Console.ReadLine().Split(" ");
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+        }
+    }
+}
