f28d789ed94fa81ff9b4b32f9811ddad0a1b4767
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 21:58:41 -0300

    Add files via upload

---
+++ Exercício 1037/Exercicio1037_Judge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1037_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1037
+             *
+             */
+
+            double valor = double.Parse(Console.ReadLine());
+
+            if (valor >= 0.250 && valor <= 25.000)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (valor >= 25.0001 && valor <= 50.000)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (valor >= 50.001 && valor <= 75.000)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else if (valor >= 75.001 && valor <= 100.000)
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+            else
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+        }
+    }
+}
