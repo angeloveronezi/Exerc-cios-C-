014d60a398c41e5b02bc6edeff67279b87c70ed9
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-22 23:15:10 -0300

    Add files via upload

---
+++ Exercicio 1051/Exercicio1051_Judge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1051_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1051
+             *
+             */
+
+            double renda = 0.0, imposto = 0.0;
+
+            renda = double.Parse(Console.ReadLine());
+
+            if (renda > 0.00 && renda < 2000.00)
+            {
+                Console.WriteLine("Isento");
+            }
+            else if (renda > 2000.01 && < 3000.00)
+            {
+
+            }
+        }
+    }
+}
