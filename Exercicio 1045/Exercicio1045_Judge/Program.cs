0d7f23555c16298665df98cbe05759e16c650a96
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-22 22:43:31 -0300

    Add files via upload

---
+++ Exercicio 1045/Exercicio1045_Judge/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicio1045_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * https://judge.beecrowd.com/pt
+             * 
+             * Exercício 1045
+             */
+            string[] vet = Console.ReadLine().Split(" ");
+            double a = 0.0, b = 0.0, c = 0.0, A = 0.00, B = 0.00, C = 0.00;
+
+            A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            B = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            if (A > B && A > C)
+            {
+                a = A;
+                if (B > C)
+                {
+                    b = B;
+                    c = C;
+                }
+                else
+                {
+                    c = B;
+                    C = b;
+                }
+            }
+            else if (B > A && B > C)
+            {
+                a = B;
+                if (A > C)
+                {
+                    b = A;
+                    c = C;
+                }
+                else
+                {
+                    b = C;
+                    c = B;
+                }
+            }
+            else
+            {
+                a = C;
+                if (A > C)
+                {
+                    b = A;
+                    c = B;
+                }
+                else
+                {
+                    b = B;
+                    c = A;
+                }
+            }
+
+
+            if (a >= b + c)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else if ((a * a) == ((b * b) + (c * c)))
+            {
+                Console.WriteLine("TRIANGULO RETANGULO");
+            }
+            else if ((a * a) > ((b * b) + (c * c)))
+            {
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+            }
+            else if ((a * a) < ((b * b) + (c * c)))
+            {
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+            }
+
+
+            if (a == b && b == c && c == a)
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO");
+            }
+            else if (a == b || b == c || c == a)
+            {
+                Console.WriteLine("TRIANGULO ISOSCELES");
+            }
+        }
+    }
+}
---
+++ Exercicio 1047/Exercicio1047_Judge/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1047_Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * * https://judge.beecrowd.com/pt
+             * 
+             * Exercícío 1047
+             *
+             */
+
+            int h1, h2, min1, min2;
+            string[] vet = Console.ReadLine().Split(" ");
+
+            h1 = int.Parse(vet[0]);
+            min1 = int.Parse(vet[1]);
+            h2 = int.Parse(vet[2]);
+            min2 = int.Parse(vet[3]);
+
+            //Transformando horas em minutos
+            h1 *= 60;
+            h2 *= 60;
+
+            //Somando os minutos em horas
+            h1 += min1;
+            h2 += min2;
+
+            if (h2 - h1 == 0)
+            {
+                Console.WriteLine("O JOGO DUROU 24 HORA (S) E 0 MINUTO (S)");
+            }
+            else if (h2 - h1 < 60)
+            {
+                Console.WriteLine("O JOGO DUROU 0 HORA (S) E " + (h2 - h1) + " MINUTO (S)");
+            }
+            else
+            {
+                int total = h2 - h1;
+                int resto = total % 60;
+                Console.WriteLine("O JOGO DUROU " + resto + " HORA (S) E " + ((total - resto) / 60) + " MINUTO (S)");
+            }
+        }
+    }
+}
